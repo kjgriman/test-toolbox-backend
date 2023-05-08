004f028c7096db6af7365eccae2eae8d9d22875f
kerbin-dev <kerbin@sweetcherrytech.com>
2023-05-08 19:32:53 -0400

    change message responde welcome api

--- components/welcome/services/index.js
+++ components/welcome/services/index.js
@@ -8,7 +8,7 @@ exports.servicesWelcome = async () => {
     try {
         let message = {
             message:
-                'Welcome to backend test toolbox, please add /api/v1 into the url',
+            'Welcome to backend test toolbox, please add /api/v1 into the url then /api/v1/docs to see the documentation',
         }
         return message
     } catch (err) {
